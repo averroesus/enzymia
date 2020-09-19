9f202a9f25181b43abfcc6bf81be08fa27a66f60
edition13 <edition13@yahoo.com>
2020-09-19 10:40:20 +0300

    added AJAX

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 var box = document.getElementsByClassName('question-box');
+
+const reqQuiz = new XMLHttpRequest();
+reqQuiz.open('GET', '/quizes(json)/quiz1.json');
+reqQuiz.onload = function () {
+  const quizContent = JSON.parse(reqQuiz.responseText);
+  console.log(quizContent);
+};
+
+reqQuiz.send();
+
+console.log('hello');
